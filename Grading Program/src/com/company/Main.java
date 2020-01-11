28c61d09c5967e940eb15db47497540827016b17
akaashnkapoor <akaashkapoor@cmail.carleton.ca>
2020-01-11 18:37:27 -0500

    Source files for Grading Program

--- Grading Program/src/com/company/Main.java
+++ Grading Program/src/com/company/Main.java
@@ -1,12 +1,11 @@
 package com.company;
 
-public class Main {
+public class Main { //The main class runs the actual program by using methods from StudentRecords class. 
 
     public static void main(String[] args) {
-        StudentRecord Student = new StudentRecord();
+        StudentRecord Student = new StudentRecord();//creation of the Student object. 
         Student.readInput();
         Student.writeOutput();
-
-
+        
     }
 }
--- Grading Program/src/com/company/StudentRecord.java
+++ Grading Program/src/com/company/StudentRecord.java
@@ -1,13 +1,23 @@
-package com.company;
-import java.util.Scanner;
+/*
+    Program created by: Akaash Kapoor
+    Date: January 10th, 2020
+    Description: Applying concepts learned from school into a Grade Calculating program.
+    Weights for the quizzes are 25% all together each marked out of 10. 
+    Weight for the midterm is 50% marked out of 100.
+    Weight for the final is 50% marked out of 100. 
+    Room for Improvement: Create my own grade calcualtor so I could apply it to future courses. 
+*/
 
-public class StudentRecord {
-    private double quizScore1;
+package com.company;
+import java.util.Scanner; //User input library
+
+public class StudentRecord { 
+    private double quizScore1; //Private instance variables means the variables can't be access from the main program (Encapsulation).
     private double quizScore2;
     private double midtermScore;
     private double finalScore;
 
-    Scanner keyboard = new Scanner(System.in);
+    Scanner keyboard = new Scanner(System.in);//Creates a new object for user input. 
 
     public StudentRecord(){ //Default constructor
          quizScore1 = 0;
@@ -16,7 +26,7 @@ public class StudentRecord {
          finalScore = 0;
     }
 
-    public void readInput(){
+    public void readInput(){ //Method for reading inputs
         System.out.println("What was the Score on the first Quiz: ");
         quizScore1 = keyboard.nextDouble();
         System.out.println("What was the Score on the second Quiz: ");
@@ -26,24 +36,24 @@ public class StudentRecord {
         System.out.println("What was the score on the Final: ");
         finalScore = keyboard.nextDouble();
     }
-    private double overallGrade(){
+    private double overallGrade(){ //Method for returning the final grade after all computations have been complete.
         return quizGrade(quizScore1, quizScore2) + midtermGrade(midtermScore)+ finalGrade(finalScore);
     }
 
-    private double quizGrade(double quizScore1, double quizScore2){
+    private double quizGrade(double quizScore1, double quizScore2){ //Calculates the weight of the quiz grade. 
         return ((quizScore1 + quizScore2) / 20.0) * 25;
     }
 
-    private double midtermGrade(double midtermScore){
+    private double midtermGrade(double midtermScore){ //Calculates the weight of the midterm grade.
         return (midtermScore/100.0) * 25;
     }
 
-    private double finalGrade(double finalScore){
+    private double finalGrade(double finalScore){//Calculates the weight of the final grade. 
         return (finalScore/100.0) * 50;
     }
 
 
-   private char finalGrade() {
+   private char finalGrade() { //Returns the letter grade in terms of a letter. 
        double finalGrade = overallGrade();
        if (finalGrade < 60) {
            return 'F';
@@ -59,7 +69,7 @@ public class StudentRecord {
 
    }
 
-    public void writeOutput (){
+    public void writeOutput (){ //outputs the student record. 
         System.out.println("Student Record");
         System.out.println("-------------------------------------");
         System.out.println("The quiz percentage weight was: " + quizGrade(quizScore1,quizScore2)+ " %");
